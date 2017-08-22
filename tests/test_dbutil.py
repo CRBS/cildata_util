1af985281786e78d1f2beb2ae0ded5532a4c961d
Chris Churas <churas@gmail.com>
2017-08-22 16:30:45 -0700

    added tests to verify jsonpickle reader and writer

--- tests/test_dbutil.py
+++ tests/test_dbutil.py
@@ -13,7 +13,13 @@ import time
 
 from cildata_util.dbutil import Database
 from cildata_util.dbutil import CILDataFile
+from cildata_util.dbutil import CILDataFileJsonPickleWriter
+from cildata_util.dbutil import CILDataFileListFromJsonPickleFactory
 
+class FakeCILDataFile(object):
+    """Fake object
+    """
+    pass
 
 class TestDbutil(unittest.TestCase):
     """Tests for `cildatadownloader` package."""
@@ -24,6 +30,19 @@ class TestDbutil(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
+    def test_database_get_alternate_connection(self):
+        db = Database(None)
+        db.set_alternate_connection('hi')
+        self.assertEqual(db.get_connection(), 'hi')
+
+    def test_database_with_none_for_config(self):
+        db = Database(None)
+        try:
+            db.get_connection()
+        except AttributeError as e:
+            self.assertEqual(str(e),
+                             "'NoneType' object has no attribute 'get_host'")
+
     def test_cildatafile(self):
         cdf = CILDataFile(1)
         self.assertEqual(cdf.get_id(), 1)
@@ -33,6 +52,9 @@ class TestDbutil(unittest.TestCase):
         self.assertEqual(cdf.get_file_name(), None)
         self.assertEqual(cdf.get_is_video(), None)
         self.assertEqual(cdf.get_mime_type(), None)
+        self.assertEqual(cdf.get_headers(), None)
+        self.assertEqual(cdf.get_file_size(), None)
+        self.assertEqual(cdf.get_localfile(), None)
 
         cdf.set_checksum('123')
         cdf.set_download_success(True)
@@ -40,6 +62,9 @@ class TestDbutil(unittest.TestCase):
         cdf.set_file_name('somefile')
         cdf.set_is_video(False)
         cdf.set_mime_type('mimetype')
+        cdf.set_headers(['hi'])
+        cdf.set_file_size(123)
+        cdf.set_localfile('local')
 
         self.assertEqual(cdf.get_id(), 1)
         self.assertEqual(cdf.get_checksum(), '123')
@@ -48,8 +73,91 @@ class TestDbutil(unittest.TestCase):
         self.assertEqual(cdf.get_file_name(), 'somefile')
         self.assertEqual(cdf.get_is_video(), False)
         self.assertEqual(cdf.get_mime_type(), 'mimetype')
+        self.assertEqual(cdf.get_headers(), ['hi'])
+        self.assertEqual(cdf.get_file_size(), 123)
+        self.assertEqual(cdf.get_localfile(), 'local')
 
+    def test_cildatafile_copy(self):
+        fakecdf = FakeCILDataFile()
+        cdf = CILDataFile(123)
+        cdf.copy(fakecdf)
+        self.assertEqual(cdf.get_id(), 123)
+        self.assertEqual(cdf.get_checksum(), None)
+        self.assertEqual(cdf.get_download_success(), None)
+        self.assertEqual(cdf.get_download_time(), None)
+        self.assertEqual(cdf.get_file_name(), None)
+        self.assertEqual(cdf.get_is_video(), None)
+        self.assertEqual(cdf.get_mime_type(), None)
+        self.assertEqual(cdf.get_headers(), None)
+        self.assertEqual(cdf.get_file_size(), None)
 
+    def test_cildatafilepicklewriter_writeonecdf(self):
+        cdf = CILDataFile(123)
+        cdf.set_checksum('123')
+        cdf.set_download_success(True)
+        cdf.set_download_time(0)
+        cdf.set_file_name('somefile')
+        cdf.set_is_video(False)
+        cdf.set_mime_type('mimetype')
+        cdf.set_headers(['hi'])
+        cdf.set_file_size(123)
 
+        cdf_list = []
+        cdf_list.append(cdf)
+        temp_dir = tempfile.mkdtemp()
+        try:
+            writer = CILDataFileJsonPickleWriter()
+            outfile = os.path.join(temp_dir, 'foo')
+            writer.writeCILDataFileListToFile(outfile, cdf_list)
+            reader = CILDataFileListFromJsonPickleFactory()
 
+            suffixoutfile = outfile + CILDataFileJsonPickleWriter.SUFFIX
+            res_list = reader.get_cildatafiles(suffixoutfile)
 
+            self.assertEqual(res_list[0].get_id(), cdf_list[0].get_id())
+            self.assertEqual(res_list[0].get_file_name(),
+                             cdf_list[0].get_file_name())
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_cildatafilepicklewriter_writetwocdfs(self):
+        cdf = CILDataFile(123)
+        cdf.set_checksum('123')
+        cdf.set_download_success(True)
+        cdf.set_download_time(0)
+        cdf.set_file_name('somefile')
+        cdf.set_is_video(False)
+        cdf.set_mime_type('mimetype')
+        cdf.set_headers(['hi'])
+        cdf.set_file_size(123)
+
+        cdf_list = []
+        cdf_list.append(cdf)
+
+        cdf2 = CILDataFile(3465)
+        cdf_list.append(cdf2)
+        temp_dir = tempfile.mkdtemp()
+        try:
+            writer = CILDataFileJsonPickleWriter()
+            outfile = os.path.join(temp_dir, 'foo')
+            writer.writeCILDataFileListToFile(outfile, cdf_list)
+            reader = CILDataFileListFromJsonPickleFactory()
+
+            suffixoutfile = outfile + CILDataFileJsonPickleWriter.SUFFIX
+            res_list = reader.get_cildatafiles(suffixoutfile)
+
+            self.assertEqual(res_list[0].get_id(), cdf_list[0].get_id())
+            self.assertEqual(res_list[0].get_file_name(),
+                             cdf_list[0].get_file_name())
+            self.assertEqual(res_list[1].get_id(), cdf_list[1].get_id())
+            self.assertEqual(res_list[1].get_file_name(),
+                             cdf_list[1].get_file_name())
+        finally:
+            shutil.rmtree(temp_dir)
+
+    # def test_read_old_json(self):
+    #    reader = CILDataFileListFromJsonPickleFactory()
+        # cdf_list = reader.get_cildatafiles('/home/churas/src/CIL_file_download_tool/39602.json')
+        # cdf_list = reader.get_cildatafiles('/home/churas/tasks/cil_downloader/out/images/10001/10001.json')
+        # self.assertEqual(cdf_list[0].get_file_size(), None)
+        # self.assertEqual(cdf_list[0].get_headers()['_store']['content-type']['py/tuple'][1], 'image/tif')
